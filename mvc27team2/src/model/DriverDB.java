24e66312d4e3313e4d26f7a77d263423933ea4d4
smart-202-19 <smart-202-19@smart-202-19-PC>
2018-03-09 13:47:18 +0900

    [나성수]DriverDB클래스 생성 및 mysql드라이브로딩 및 DB접근 내용 작성

---
+++ mvc27team2/src/model/DriverDB.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverDB {
+	
+	public Connection connection;
+	
+	public Connection driverDB() throws ClassNotFoundException, SQLException {
+		System.out.println("드라이버 로딩 및 DB연결 driverDB 실행");
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		String jdbcAddr = "jdbc:mysql://localhost:3306/mvc?useUnicode=true&characterEncoding=utf8";
+		String dbId = "root";
+		String dbPw = "java0000";
+		
+		connection = DriverManager.getConnection(jdbcAddr, dbId, dbPw);
+		
+		return connection;
+	}
+}
